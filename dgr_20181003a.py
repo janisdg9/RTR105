d6106b67f908839073ce71a4d0bd27f100cfbe21
user <jaaannis1997@gmail.com>
2018-10-03 10:44:47 +0300

    20181003_10_44

---
+++ dgr_20181003a.py
@@ -0,0 +1,95 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print(123)
+123
+>>> print(98.6)
+98.6
+>>> print('Hello')
+Hello
+>>> x=12.2
+>>> y=14
+>>> x=100
+>>> x=2
+>>> x=x+2
+>>> print(x)
+4
+>>> xx=2
+>>> xx=xx+2
+>>> print(xx)
+4
+>>> vars
+<built-in function vars>
+>>> a10=10
+>>> type(input("Ievadi: "))
+Ievadi: 
+
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    type(input("Ievadi: "))
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> yy=440*12
+>>> print(yy)
+5280
+>>> zz=yy/1000
+>>> print(zz)
+5
+>>> x=1+2**3/4*5
+>>> print(x)
+11
+>>> ddd=1+4
+>>> print(ddd)
+5
+>>> type(eee)
+
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    type(eee)
+NameError: name 'eee' is not defined
+>>> xx=1
+>>> type(xx)
+<type 'int'>
+>>> temp=98.6
+>>> type(temp)
+<type 'float'>
+>>> type(1)
+<type 'int'>
+>>> type(1.0)
+<type 'float'>
+>>> print(float(99)+100)
+199.0
+>>> i=42
+>>> type(i)
+<type 'int'>
+>>> f=float(i)
+>>> print(f)
+42.0
+>>> type(f)
+<type 'float'>
+>>> print(10/2)
+5
+>>> print(9/2)
+4
+>>> sval='¹23'
+>>> type(sval)
+<type 'str'>
+>>> print(sval+1)
+
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    print(sval+1)
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> ival=int(sval)
+
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    ival=int(sval)
+ValueError: invalid literal for int() with base 10: '\xc2\xb923'
+>>> 
+
+
+
+
