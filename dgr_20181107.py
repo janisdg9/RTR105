28e7a0c79fcadbdee72511a65cb0082f3bf6615d
janisdg9 <42992942+janisdg9@users.noreply.github.com>
2018-11-07 09:45:28 +0200

    20181107_09_45

---
+++ dgr_20181107.py
@@ -0,0 +1,14 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> def mans_sinuss(x):
+	k = 0
+	a = (-1)**0*x**1/(1)
+	S = a
+	print("Izdruka no liet.f. a0 = \6.2f S0 = \6.2f"%(a,S))
+
+	
+>>> while k< 4:
+	k = k + 1
+	R = (-1)*x*x/((2*k)*(2*k+1))
+	a =
